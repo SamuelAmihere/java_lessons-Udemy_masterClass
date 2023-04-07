102d4bd44963e29c90bced8c85a1ca15e7fc8d4e
SamuelAmihere <amisam2000@gmail.com>
2023-04-07 17:27:24 +0000

    first commit

---
+++ Collections/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int lastRow = 'A'+10;
+        for(char row='A'; row <= lastRow; row++){
+            System.out.println("Row NO: "+row);
+        }
+    }
+}
